285e8096b00b76a0c6c1ce1cd4410d16464c0de8
Rajdeep Ghatore <r.ghatore@gmail.com>
2021-03-08 12:57:52 -0700

    injecting logging service into components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
+// we can also provide services in module for it to be used in any component
+// import { LoggingService } from 'src/shared/logging.service';
 
 @NgModule({
   declarations: [
