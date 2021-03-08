18a02c659e3a72fd114b3d2b115f90b5247002f7
Rajdeep Ghatore <r.ghatore@gmail.com>
2021-03-08 14:06:31 -0700

    adding a data service

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,27 +1,30 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
+import { AccountsService } from 'src/shared/accounts.service';
 import { LoggingService } from 'src/shared/logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  providers: [
+    LoggingService,
+    // this doesn't update the apps instance because
+    // it creates a new instance separate from the app instance
+    // AccountsService
+  ]
 })
 
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-  constructor(private loggingService: LoggingService) {};
+  constructor(
+    private loggingService: LoggingService,
+    private accountsService: AccountsService
+    ) {};
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-
+    this.accountsService.onAccountAdded({name: accountName, status: accountStatus});
     this.loggingService.logStatusChange(accountStatus);
   }
-
 
 }
---
+++ src/shared/accounts.service.ts
@@ -0,0 +1,24 @@
+export class AccountsService {
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
+    }
+  ];
+
+  onAccountAdded(newAccount: {name: string, status: string}) {
+    this.accounts.push(newAccount);
+  }
+
+  onStatusChanged(id: number, newStatus: string) {
+    this.accounts[id].status = newStatus;
+  }
+}
